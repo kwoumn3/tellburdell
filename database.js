bcf31c757df15ab91ffeba5f6dc03c9091d9cd7d
driazati <riazati.david@gmail.com>
2015-10-14 23:04:42 -0400

    form submission internals

--- database.js
+++ database.js
@@ -9,3 +9,7 @@ console.log("got new db");
 exports.query = function(sql, callback) {
 	db.all(sql, callback);
 };
+
+exports.run = function(sql, params, callback) {
+	db.run(sql, callback);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+	handleForm(req.body);
+	res.render('thanks', { title: 'Thanks' });
+});
+
+function handleForm(body) {
+	//TODO: sanitize input
+	var sql = "INSERT INTO feedback (department_id, name, text) VALUES (2,'" + body.name + "','" + body.feedback + "');"
+	var params = {
+		name: body.name
+	};
+	params.$department_id = 2;
+	params.$name = body.name;
+	params.$feedback = body.feedback;
+	database.run(sql, params, function(err) {
+		if (err == null) {
+			console.log("Insert successful");
+		} else {
+			console.log("Error: " + err);
+		}
+	});
+}
 
 module.exports = router;
